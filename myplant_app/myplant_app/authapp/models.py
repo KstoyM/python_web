e41697a0f1c3e96b25ae67260087b1f114912d17
KstoyM <114015829+KstoyM@users.noreply.github.com>
2023-06-18 15:00:17 +0300

    Delete myplant_app directory

--- myplant_app/myplant_app/authapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class ProfileModel(models.Model):
-    username = models.CharField(max_length=30)  # TODO: validators
-    first_name = models.CharField()
